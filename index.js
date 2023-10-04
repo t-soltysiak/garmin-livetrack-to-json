de569d065faef1642e2730cb8030864708d8644d
Tomasz Sołtysiak <68973012+t-soltysiak@users.noreply.github.com>
2023-10-04 12:50:13 +0200

    bugfix for logs of position

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ const fetchData = async (id, token, res) => {
     finished = sessionData.trackPoints[sessionData.trackPoints.length-1].fitnessPointData.eventTypes[1] === 'END';
     log.info(finished ? 'Activity is FINISHED' : 'Activity is ONGOING');
     log.info('Getting last position reverse geocode address');
-    log.info('Latitude: '+lastPosition.lat+' Longitude:' + lastPosition.lon);
     const lastPosition = sessionData.trackPoints[sessionData.trackPoints.length-1].position;
+    log.info('Latitude: '+lastPosition.lat+' Longitude:' + lastPosition.lon);
     const reverse = await geocoder.reverse({ lat: lastPosition.lat, lon: lastPosition.lon });
     log.info(reverse.address);
     const sessionDataWithUrl = {
