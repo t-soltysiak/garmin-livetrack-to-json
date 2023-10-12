a1e7c7a1a6ead200051e6f7c102b9f7e3794e668
Tomasz Sołtysiak <68973012+t-soltysiak@users.noreply.github.com>
2023-10-12 19:51:13 +0200

    bugix for address - check if streetName is undefined etc.

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const fetchData = async (id, token, res) => {
     const sessionDataWithUrl = {
       ...{
         "sessionUrl": `https://livetrack.garmin.com/session/${id}/token/${token}`,
-        "positionAddress": `${city}, ${config.geocoderStreet} ${streetName}${streetNumber ? ` ${streetNumber}` : ''}`,
+        "positionAddress": `${city}${streetName ? `, ${config.geocoderStreet} ${streetName}` : ''}${streetName && streetNumber ? ` ${streetNumber}` : ''}`,
       },
       ...sessionData
     }
